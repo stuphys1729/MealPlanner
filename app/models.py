11ef1b247f3fe692023f8948e561197efd9e024a
stuphys1729 <s1432366@sms.ed.ac.uk>
2019-07-27 22:40:47 +0100

    Added names to recipes

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    recipes = db.relationship('Recipe', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -13,6 +14,7 @@ class User(db.Model):
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
     ingredients = db.Column(db.String(140))
     method = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
