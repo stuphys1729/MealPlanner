bbf7db1cb5bd49efacb1d751d788f047d87f85d5
stuphys1729 <s1432366@sms.ed.ac.uk>
2019-07-27 19:30:05 +0100

    First bits of website (following https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates )

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,25 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Jillian'}
+    recipes = [
+        {
+            'name' : 'Chilli Chicken',
+            'ingredients' : [
+                ('Spring Onion', 1),
+                ('Baby Corn', 1)
+            ]
+        },
+        {
+            'name' : 'Stir Fry',
+            'ingredients' : [
+                ('Onion', 1),
+                ('Baby Corn', 1)
+            ]
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, recipes=recipes)
