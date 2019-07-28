78ae5e2a5865fed01067dd4a718a3724329bb051
stuphys1729 <s1432366@sms.ed.ac.uk>
2019-07-28 11:47:36 +0100

    Added user registration and enforced login

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,18 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
+
+# Initialise app with config settings
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Initialise user login settings
+login = LoginManager(app)
+login.login_view = 'login'
+
+# Initialise database and migrations
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
